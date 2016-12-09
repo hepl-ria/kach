347dcaa04a7a7d524deeb90662ca1c09d9d97260
leny <info@flatland.be>
2016-12-09 12:23:00 +0100

    Install dependencies & add base dummy modules

---
+++ static/modules/utils.js
@@ -0,0 +1,11 @@
+/* leny/kach
+ *
+ * /static/modules/utils.js - Small utils
+ *
+ * coded by leny@flatLand!
+ * started at 09/12/2016
+ */
+
+export default function() {
+    console.log( "Hey from utils!" );
+}
