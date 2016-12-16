bccebc6e8b1143a60a27443daaa4eb676ae7063a
leny <info@flatland.be>
2016-12-16 10:25:55 +0100

    Add uglify to modules

--- gulpfile.js
+++ gulpfile.js
@@ -19,6 +19,9 @@ var gulp = require( "gulp" ),
     browserify = require( "browserify" ),
     babelify = require( "babelify" ),
     sourceStream = require( "vinyl-source-stream" ),
+    buffer = require( "vinyl-buffer" ),
+    gRename = require( "gulp-rename" ),
+    gUglify = require( "gulp-uglify" ),
     ObjectID = Mongo.ObjectID,
     MongoClient = Mongo.MongoClient;
 
@@ -108,6 +111,10 @@ gulp.task( "modules", function() {
         } )
         .bundle()
         .pipe( sourceStream( "app.js" ) )
+        .pipe( gulp.dest( "static/js/" ) )
+        .pipe( buffer() )
+        .pipe( gRename( "app.min.js" ) )
+        .pipe( gUglify().on( "error", console.log ) )
         .pipe( gulp.dest( "static/js/" ) );
 } );
 
